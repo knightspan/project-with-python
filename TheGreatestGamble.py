e339ac0d29a6a8df9d3ab716dd7bfb91b2af1730
knightspan <140009343+knightspan@users.noreply.github.com>
2025-04-10 01:03:41 +0530

    Update TheGreatestGamble.py

--- TheGreatestGamble.py
+++ TheGreatestGamble.py
@@ -1,3 +1,8 @@
+MAX_LINES = 3
+MAX_BET = 100
+MIN_BET= 1
+
+
 def deposit():
     while True:
         amount=input("What would you like to deposit>? $")
@@ -14,7 +19,28 @@ def deposit():
     return amount
 
 
-deposit()
+def get_number_of_lines():
+
+        while True:
+            lines = input("Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")? ")
+            if lines.isdigit():
+                lines = int(lines)
+                if 1 <= lines <= MAX_LINES:
+                    break
+                else:
+                    print("Enter valid number of lines")
+            else:
+                print("Are you dumb mate?enter a fricking number!")
+
+        return lines
+
+def main():
+
+    balance = deposit()
+    lines = get_number_of_lines()
+    print(balance,lines)
+
+main()
 
 
 

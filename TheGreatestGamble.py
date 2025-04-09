4ff5f703118681ea9a5f332ffe8099c93d512185
knightspan <140009343+knightspan@users.noreply.github.com>
2025-04-10 02:22:16 +0530

    Update TheGreatestGamble.py
    ammo loading

--- TheGreatestGamble.py
+++ TheGreatestGamble.py
@@ -1,8 +1,36 @@
+
+import random
+
 MAX_LINES = 3
 MAX_BET = 100
 MIN_BET= 1
 
+ROW = 3
+COLS = 3
 
+symbol_count = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+def get_slot_machine_spin(rows,cols,symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+
+    columns = []
+    for col in range(cols):
+        column = []
+        current_symbols = all_symbols[:]
+        for _  in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            columns.append(value)
+        columns.append(column)
+    return columns
 def deposit():
     while True:
         amount=input("What would you like to deposit>? $")
@@ -42,9 +70,9 @@ def get_bet():
             if MIN_BET <= amount <= MAX_BET:
                 break
             else:
-                print(f"Amount must be between ${MIN_BET} - ${MAX_BET}.")
+                 print(f"Amount must be between ${MIN_BET} - ${MAX_BET}.")
         else:
-            print("Are you dumb mate?enter a fricking number!")
+         print("Are you dumb mate?enter a fricking number!")
 
 
     return amount
